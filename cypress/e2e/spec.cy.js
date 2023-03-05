5fa8939e1085accfec3b8705c868d808170e67b4
Pedro Costa <pedro.vilelaa@edvisor.io>
2023-03-05 17:49:41 -0300

    Creating checkers for title and submenu verification

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,24 @@
+/// <reference types="Cypress" />
+
+import { should } from "chai"
+
 describe('spec.cy.js', () => {
-    it('should visit', () => {
-        cy.visit('/')
+    beforeEach(() => {
+        cy.visit('https://dev-bsa-dev-test.pantheonsite.io/')
+    })
+
+    it('Verify application title', function () {
+        cy.title().should('equal', 'BSA Developer Test')
+    })
+
+    it('Verify submenu list', function () {
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(1) > a > span').should('have.text', 'Welding')
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(2) > a > span').should('have.text', 'Inspection')
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(3) > a > span').should('have.text', 'Coating')
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(4) > a > span').should('have.text', 'Personnel Training')
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(5) > a > span').should('have.text', 'Consulting')
+        cy.get('#navigation-menu > ul > li.navigation-menu__item.navigation-menu__item--has-submenu > ul > li:nth-child(6) > a > span').should('have.text', 'Project Management')
+
     })
 })
+
